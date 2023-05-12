b5f5aff4e456f6131e2931e1610ecf7dd6d2b148
Yasmin Castillo <yasmin_c@outlook.com>
2023-05-11 21:16:56 -0500

    working on if and else statements

--- script.js
+++ script.js
@@ -9,15 +9,31 @@ let ties = 0;
 function playGame() {
     alert("Let's play a game!");
     var playerChoice = prompt("Choose Rock, Paper, or Scissors");
+    console.log(playerChoice);
+    var computerChoice = selectChoice();
+    console.log(computerChoice);
+    if(playerChoice === computerChoice){
+        alert("It's a tie!");
+        ties++;
+
+    } else if(playerChoice == "paper"){
+
+    }else if( playerChoice == "scissors"){
+
+    }else{
+        console.log("Invalid Input");
+        playGame();
+    }   
 }
 
 function selectChoice(){
     let computerChoice = options [Math.floor(Math.random() *3 )];
-    console.log(computerChoice);
+    //console.log(computerChoice);
+    return computerChoice;
 
 
 
 }
 
 playGame();
-selectChoice();
+
