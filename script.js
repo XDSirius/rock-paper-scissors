ffa2211e070ec92e85c6592f7e4e0adc77462aaa
Yasmin Castillo <yasmin_c@outlook.com>
2023-05-11 20:58:51 -0500

    added a way to have computer select a random choice

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let user;
 let computer;
-const options =["R","P","S"];
+const options =["rock","paper","scissors"];
 
 let wins = 0;
 let losses = 0;
 let ties = 0;
 
 function playGame() {
-    alert("HI");
+    alert("Let's play a game!");
+    var playerChoice = prompt("Choose Rock, Paper, or Scissors");
+}
+
+function selectChoice(){
+    let computerChoice = options [Math.floor(Math.random() *3 )];
+    console.log(computerChoice);
+
+
+
 }
 
 playGame();
+selectChoice();
